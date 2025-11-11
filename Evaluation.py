5ca41ff3570d9f4bb7609e7a58788438b1094828
Jvds1025 <jeroen.vandesande@hotmail.com>
2025-11-11 16:30:26 +0100

    Enhance Evaluation.py with skip accuracy calculation
    Added calculation for skip accuracy based on non-mappable predictions.

--- Evaluation.py
+++ Evaluation.py
@@ -19,7 +19,14 @@ recall = true_positives / (true_positives + false_negatives + 1e-8)
 f1 = 2 * precision * recall / (precision + recall + 1e-8)
 accuracy = (true_positives + true_negatives) / total
 
+# For graph methods include the skip accuracy | fraction of non-mappable that were correctly skipped (being under the threshold)
+Nan_correct = ((df_merged1["pred_correct"] == 0) & (df_merged1["predicted_target_field"].isna())).sum() 
+Nan_total = df_merged1["predicted_target_field"].isna().sum()
+null_total = (df_merged1["pred_correct"] == 0).sum()
+Nan_division = (Nan_correct / null_total) if Nan_total > 0 else 0.0
+
 print(f"Accuracy:  {accuracy:.3f}")
 print(f"Precision: {precision:.3f}")
 print(f"Recall:    {recall:.3f}")
 print(f"F1 Score:  {f1:.3f}")
+
